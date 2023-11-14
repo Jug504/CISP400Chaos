2f97eb796dd0919bf07e6658f48b6c264cddfbb0
Your Name <you@example.com>
2023-11-13 18:39:20 -0800

    Santiago added code cleanup and  triangle transformation

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -9,8 +9,7 @@
 using namespace sf;
 using namespace std;
 
-int main()
-{
+int main() {
     // Create a video mode object
 	VideoMode vm(1920, 1080);
 	// Create and open a window for the game
@@ -19,75 +18,49 @@ int main()
     vector<Vector2f> vertices;
     vector<Vector2f> points;
 
-	while (window.isOpen())
-	{
-        /*
-		****************************************
-		Handle the players input
-		****************************************
-		*/
-
+	while (window.isOpen()) {
+    
         sf::Font font;
-        if(!font.loadFromFile("/home/ubuntu/Desktop/cisp400/CISP400_SFML/Chapter18/fonts/Roboto-Light.ttf"))
-        {
-            std::cout << "Error loading font" << std::endl;
+        if(!font.loadFromFile("/home/ubuntu/Desktop/cisp400/CISP400_SFML/Chapter18/fonts/Roboto-Light.ttf")) {
+            cout << "Error loading font" << endl;
         }
 
-        sf::Text text;
-        text.setFont(font);
-        text.setString("Click anywhere on the screen to create three vertices of any kind of triangle, then click on another point to begin the pattern");
-        text.setCharacterSize(20);
+        sf::Text text("Click anywhere on the screen to create three vertices of any kind of triangle, then click on another point to begin the pattern",font,20);
         text.setFillColor(sf::Color::White);
         window.draw(text);
         window.display();
 
-        Event event;
-		while (window.pollEvent(event))
-		{
-            if (event.type == Event::Closed)
-            {
+        sf::Event event;
+		while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
 				// Quit the game when the window is closed
 				window.close();
             }
-            if (event.type == sf::Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == sf::Mouse::Left)
-                {
-                    std::cout << "the left button was pressed" << std::endl;
-                    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-                    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+            if (event.type == Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    cout << "the left button was pressed" << std::endl;
+                    cout << "mouse x: " << event.mouseButton.x << endl;
+                    cout << "mouse y: " << event.mouseButton.y << endl;
 
-                    if(vertices.size() < 3)
-                    {
+                    if(vertices.size() < 3) {
                         vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
                     }
-                    else if(points.size() == 0)
-                    {
+                    else if(points.size() == 0) {
                         ///fourth click
                         ///push back to points vector
                         points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y)); //Firstpoint
                     }
-                    else //Ignore all other clicks
-                    {
-                        break;
-                    }
+                    //Ignore all other clicks
                 }
             }
         }
-        if (Keyboard::isKeyPressed(Keyboard::Escape))
-		{   
+
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) {   
             //Terminate
 			window.close();
 		}
 
-        /*
-		****************************************
-		Update
-		****************************************
-		*/
-
-        if(points.size() > 0)
-        {   
+        if(points.size() > 0) {   
             ///generate more point(s)
             ///select random vertex
             ///calculate midpoint between random vertex and the last point in the vector
@@ -102,12 +75,9 @@ int main()
             }
         }
 
-        /*
-		****************************************
+		/***************************************
 		Draw
-		****************************************
-		*/
-        
+		***************************************/
         
         window.clear();
         for(unsigned int i = 0; i < vertices.size(); i++)
@@ -116,73 +86,40 @@ int main()
             rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
             rect.setFillColor(Color::White);
             window.draw(rect);
+            window.display();
         }
-        window.display();
-        for(unsigned int j = 0; j < points.size(); j++)
-        {
+
+        float aCounter,wCounter,sCounter,dCounter = 0.0;
+        for(unsigned int j = 10; j < points.size(); j++) {
             RectangleShape rect(Vector2f(2,2));
             rect.setPosition(Vector2f(points[j].x, points[j].y));
-            int color = rand() % 5;
+            int color = j % 5;
             if (color == 0) { rect.setFillColor(Color::Red); }
             else if (color == 1) { rect.setFillColor(Color::Blue); }
             else if (color == 2) { rect.setFillColor(Color::Green); }
             else if (color == 3) { rect.setFillColor(Color::Yellow);}
             else if (color == 4) { rect.setFillColor(Color::White); }
-            window.draw(rect);
-            window.display();
-        }
-        
-        
-        /*Fractal Translations via user input*/
+            
+            if(Keyboard::isKeyPressed(Keyboard::A)) { //Shift fractal to the left 
+                aCounter += 0.1f;
+                rect.setPosition(Vector2f(points[j].x - aCounter, points[j].y));
+            }
+            if(Keyboard::isKeyPressed(Keyboard::W)) { //shit fractal upwards 
+                wCounter += 0.1f;
+                rect.setPosition(Vector2f(points[j].x, points[j].y - wCounter));
+            }
+            if(Keyboard::isKeyPressed(Keyboard::S)) { //Shift fractal downwards 
+                sCounter += 0.1f;
+                rect.setPosition(Vector2f(points[j].x,points[j].y + sCounter));
+            }
+            if(Keyboard::isKeyPressed(Keyboard::D)) { //Shift fractal to the right
+                dCounter += 0.1f;
+                rect.setPosition(Vector2f(points[j].x + dCounter, points[j].y));
+            }
+                window.draw(rect);
+                window.display();
+            }
 
-        if(Keyboard::isKeyPressed(Keyboard::A))//Shift fractal to the left
-        {
-            for(unsigned int k = 0; k < points.size(); k++)
-            {
-                RectangleShape rect1(Vector2f(2,2));
-                rect1.setPosition(Vector2f(points[k].x, points[k].y));
-                rect1.move(Vector2f(-100.f, 0.f));
-                rect1.setFillColor(Color::White);
-                window.draw(rect1);
-                window.display();
-            }
-        }
-        if(Keyboard::isKeyPressed(Keyboard::W))//shit fractal upwards
-        {
-            for(unsigned int k = 0; k < points.size(); k++)
-            {
-                RectangleShape rect1(Vector2f(2,2));
-                rect1.setPosition(Vector2f(points[k].x, points[k].y));
-                rect1.move(Vector2f(0.f, -100.f));
-                rect1.setFillColor(Color::White);
-                window.draw(rect1);
-                window.display();
-            }
-        }
-        if(Keyboard::isKeyPressed(Keyboard::S))//Shift fractal downwards
-        {
-            for(unsigned int k = 0; k < points.size(); k++)
-            {
-                RectangleShape rect1(Vector2f(2,2));
-                rect1.setPosition(Vector2f(points[k].x, points[k].y));
-                rect1.move(Vector2f(0.f, 100.f));
-                rect1.setFillColor(Color::White);
-                window.draw(rect1);
-                window.display();
-            }     
-        }
-        if(Keyboard::isKeyPressed(Keyboard::D))//Shift fractal to the right
-        {
-            for(unsigned int k = 0; k < points.size(); k++)
-            {
-                RectangleShape rect1(Vector2f(2,2));
-                rect1.setPosition(Vector2f(points[k].x, points[k].y));
-                rect1.move(Vector2f(100.f, 0.f));
-                rect1.setFillColor(Color::White);
-                window.draw(rect1);
-                window.display();
-            }
-        }
         if(Keyboard::isKeyPressed(Keyboard::R)) //Meant to rotate
         {
             for(unsigned int k = 0; k < points.size(); k++)
